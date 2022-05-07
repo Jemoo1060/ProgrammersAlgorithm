c9ded6354b1e1ba6ebc369ec8b9bd6d6136626d4
jemoo <wpan1060@naver.com>
2022-05-07 13:58:43 +0900

    Add:자릿수더하기

---
+++ src/Lv1/자릿수더하기.java
@@ -0,0 +1,16 @@
+package Lv1;
+
+public class 자릿수더하기 {
+
+    public static int solution(int n) {
+
+        int answer = 0;
+
+        while(n != 0) {
+            answer += n%10;
+            n = n/10;
+        }
+
+        return answer;
+    }
+}
