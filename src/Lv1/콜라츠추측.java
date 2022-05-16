7dcc30a1b345baff3123f37b76779894af38617b
jemoo <wpan1060@naver.com>
2022-05-16 11:51:43 +0900

    Add:콜라츠추측

---
+++ src/Lv1/콜라츠추측.java
@@ -0,0 +1,30 @@
+package Lv1;
+
+public class 콜라츠추측 {
+
+    public static int solution(long num)  {
+
+
+        int answer = 0;
+
+
+        while(num != 1) {
+
+            if(num%2==0)
+                num = num/2;
+            else
+                num = (num*3)+1;
+
+            answer++;
+
+            if(answer >= 500) {
+                answer = -1;
+                break;
+            }
+
+        }
+
+
+        return answer;
+    }
+}
