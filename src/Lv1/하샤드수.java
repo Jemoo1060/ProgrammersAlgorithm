b473de17a7c443ceedb9ddbd2d600112991cfe2a
jemoo <wpan1060@naver.com>
2022-05-25 11:51:43 +0900

    Add:하샤드수

---
+++ src/Lv1/하샤드수.java
@@ -0,0 +1,24 @@
+package Lv1;
+
+public class 하샤드수 {
+
+    public static boolean solution(int x) {
+
+        boolean answer = true;
+
+
+        int temp = x;
+        int total = 0;
+
+
+        while(temp != 0) {
+            total += temp%10;
+            temp = temp/10;
+        }
+
+        if(x%total!=0 )
+            answer = false;
+
+        return answer;
+    }
+}
